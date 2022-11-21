c183b56295b18f255b0182c8d975f5d5287868be
Joshua Diaz <jad24096@gmail.com>
2022-11-21 13:25:44 -0500

    added more simple code to check if palindrome, and also fixed an issue where numbers would not be checked accurately.

--- script.js
+++ script.js
@@ -8,25 +8,31 @@ function checkInput() {
     //get the user input value and assign to a variable
     let userInput = input.value.toLowerCase() 
     //turn the input into an array and assign it to a variable
-    let forward = userInput.split('')
+    // let forward = userInput.split('')
     //create a copy of the array and reverse it 
-    let reverse = [...forward].reverse()
-    console.log(forward)
-    console.log(reverse); 
-    //loop through original array
-    for (let i = 0; i < forward.length; i++) {
-        //loop though the reversed array
-        for (let j = 0; j < reverse.length; j++) {
-            //compare each index of the two arrays
-            if (forward[i] === reverse[i]) {
-                //if the the arrays match put this response on the page
-                answer.textContent = 'yes ' + userInput + ' is a palindrome'
-            } else {
-                //if the arrays do not match put this response on the page
-                answer.textContent = 'no ' + userInput +' is not a palindrome'
-            }
-        }
+    // let reverse = [...forward].reverse()
+    // console.log(forward)
+    // console.log(reverse); 
+    // //loop through original array
+    // for (let i = 0; i < forward.length; i++) {
+    //     //loop though the reversed array
+    //     for (let j = 0; j < reverse.length; j++) {
+    //         //compare each index of the two arrays
+    //         if (forward[i] === reverse[j]) {
+    //             //if the the arrays match put this response on the page
+    //             answer.textContent = 'yes ' + userInput + ' is a palindrome'
+    //         } else {
+    //             //if the arrays do not match put this response on the page
+    //             answer.textContent = 'no ' + userInput +' is not a palindrome'
+    //         }
+    //     }
+    // }
+    if (userInput === userInput.split('').reverse().join('')) {
+        return answer.textContent = 'yes ' + userInput + ' is a palindrome';
+    } else {
+        return answer.textContent = 'no ' + userInput +' is not a palindrome'
     }
+    
 }
 
 //when the user hits submit the checkinput function will be triggered
